dd8529ef6a2b5c0c9965d290382382ebc3dc89d8
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-10-26 15:29:31 -0300

    added #339A

---
+++ 339A - Helpful Maths/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main() {
+	string expression;
+	cin >> expression;
+
+	int amount = ceil(expression.size() / 2.0);
+	sort(expression.begin(), expression.end());
+
+	for (int i = 0; i < amount; i++) {
+		cout << expression[expression.size() - amount + i] << (i < amount - 1 ? "+" : "");
+	}
+}
