95aa6ac56415eabd8866dc1738a9b5ee2276a5a2
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-04-18 22:43:18 -0300

    added #236A

---
+++ 236A - Boy or Girl/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string input;
+	cin >> input;
+
+	int max = 0;
+	char chars[100];
+
+	for (int index = 0; index < input.length(); index++) {
+		char character = input[index];
+		bool found = false;
+
+		for (int i = 0; i < max; i++) {
+			if (chars[i] == character) {
+				found = true;
+				break;
+			}
+		}
+
+		if (!found) {
+			chars[max++] = character;
+		}
+	}
+
+	cout << (max % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
+}
