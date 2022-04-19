df27307099eedfac13dfcc3461dda8bea4cbeead
JeanExtreme002 <jeangamerextreme@gmail.com>
2022-04-18 22:49:36 -0300

    fixed tabs

--- 236A - Boy or Girl/main.cpp
+++ 236A - Boy or Girl/main.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 
 int main() {
-	string input;
-	cin >> input;
+    string input;
+    cin >> input;
 
-	int max = 0;
-	char chars[100];
+    int max = 0;
+    char chars[100];
 
-	for (int index = 0; index < input.length(); index++) {
-		char character = input[index];
-		bool found = false;
+    for (int index = 0; index < input.length(); index++) {
+        char character = input[index];
+        bool found = false;
 
-		for (int i = 0; i < max; i++) {
-			if (chars[i] == character) {
-				found = true;
-				break;
-			}
-		}
+        for (int i = 0; i < max; i++) {
+            if (chars[i] == character) {
+                found = true;
+                break;
+            }
+        }
 
-		if (!found) {
-			chars[max++] = character;
-		}
-	}
+        if (!found) {
+            chars[max++] = character;
+        }
+    }
 
-	cout << (max % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
+    cout << (max % 2 == 0 ? "CHAT WITH HER!" : "IGNORE HIM!");
 }
